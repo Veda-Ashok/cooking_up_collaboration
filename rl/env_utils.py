398b9800455fdac22e9db25231f68f9cbd56316b
Veda <vashok061@gmail.com>
2026-02-25 15:00:29 -0800

    added mlp agent as the partner in the rl environment. both agents move around, but do not score rewards. will look into why mlp agent doesnt score next.

--- rl/env_utils.py
+++ rl/env_utils.py
@@ -10,32 +10,37 @@ from imitation.preprocessing import _configure_planner_cache
 class OvercookedRLWrapper(gym.Env):
     " Wrapper class to make overcooked-ai into gym env using same featurization as imitation learning"
 
-    def __init__(self, layout_name, planner_cache_dir = './planner_cache'):
+    def __init__(self, layout_name, gym_partner = None, planner_cache_dir = './planner_cache'):
 
         super().__init__()
         _configure_planner_cache(planner_cache_dir)
 
         self.mdp = OvercookedGridworld.from_layout_name(layout_name)
         self.mlam = MediumLevelActionManager.from_pickle_or_compute(self.mdp, NO_COUNTERS_PARAMS, force_compute = False)
-        self.env = OvercookedEnv.from_mdp(self.mdp, horizon = 400)
-        self.actions = gym.spaces.Discrete(6) # up, down, right, left, stay, interact
+        self.env = OvercookedEnv.from_mdp(self.mdp, horizon = 1200) # benchmark uses a horizon of 400. Setting to 1200 for testing purposes.
 
         dummy_state = self.mdp.get_standard_start_state()
         dummy_feat = self.mdp.featurize_state(dummy_state, self.mlam)[0]
-        self.observations = gym.spaces.Box(low = -np.inf, high = np.inf, shape = (len(dummy_feat), ), dtype = np.float32)
+
+        self.action_space = gym.spaces.Discrete(6) # up, down, right, left, stay, interact
+        self.observation_space = gym.spaces.Box(low = -np.inf, high = np.inf, shape = (len(dummy_feat), ), dtype = np.float32)
     
+        self.gym_partner = gym_partner
+        self.latest_obs_p1 = None
+
     def _get_observations(self, state):
 
         feats = self.mdp.featurize_state(state, self.mlam)
 
         return np.array(feats[0], dtype = np.float32), np.array(feats[1], dtype = np.float32)
     
-    def reset(self):
-
+    def reset(self, seed = None):
+        super().reset(seed = seed) # reproducibility
         self.env.reset()
         obs_p0, obs_p1 = self._get_observations(self.env.state)
+        self.latest_obs_p1 = obs_p0 # mlp model was trained as p0, so maybe we feed the gym partner p0's observations
 
-        return obs_p0, obs_p1
+        return obs_p0, {}
     
     def step(self, action_p0):
 
@@ -43,12 +48,24 @@ class OvercookedRLWrapper(gym.Env):
             idx_p0 = int(action_p0[0])
         else:
             idx_p0 = int(action_p0) 
-
-        action_p0_conv = Action.INDEX_TO_ACTION[idx_p0]
-        action_p1_conv = Action.STAY
-
-        combined_action = (action_p0_conv, action_p1_conv)
-        next_state, reward, done, info = self.env.step(combined_action)
-        obs_p0, obs_p1 = self._get_observations(next_state)
         
-        return obs_p0, reward, done, info
+    
+        action_p0_conv = Action.INDEX_TO_ACTION[idx_p0]
+        
+        if self.gym_partner:
+            with torch.no_grad():
+                obs_tensor = torch.tensor(self.latest_obs_p1, dtype = torch.float32).unsqueeze(0)
+                pred = self.gym_partner(obs_tensor)
+                idx_p1 = torch.argmax(pred, dim=1).item()
+            action_p1_conv = Action.INDEX_TO_ACTION[idx_p1]
+        else:
+            action_p1_conv = Action.STAY
+        
+        combined_action = (action_p0_conv, action_p1_conv)
+        next_state, reward, term, info = self.env.step(combined_action)
+        obs_p0, obs_p1 = self._get_observations(next_state)
+        self.latest_obs_p1 = obs_p0
+        trunc = False # gym's step function expects 5 return values
+
+        print(f'P0 Action: {action_p0_conv} | P1 Action: {action_p1_conv} | Reward: {reward}')
+        return obs_p0, reward, term, trunc, info
--- tests/rl/test_env.py
+++ tests/rl/test_env.py
@@ -1,16 +1,37 @@
 import numpy as np
+import torch
 from rl.env_utils import OvercookedRLWrapper
+from imitation.models.mlp import MLPPolicy
 
 env = OvercookedRLWrapper("cramped_room")
+din = env.observation_space.shape[0]
+dout = env.action_space.n
+
+gp = MLPPolicy(input_dim = din, num_actions = dout)
+checkpoint = torch.load('trained_models/bc/mlp_partner/best.pt')
+state_dict = checkpoint['model_state_dict']
+if not isinstance(state_dict, dict):
+    state_dict = state_dict.state_dict()
+gp.load_state_dict(state_dict)
+gp.eval()
+env.gym_partner = gp
+
+# env = OvercookedRLWrapper("cramped_room", gym_partner = gp)
 obs, info = env.reset()
 print('Observation Shape: ', obs.shape)
 
 total_reward = 0
-for step in range(400):
-    obs, reward, done, info = env.step(env.actions.sample())
-    total_reward += 1 # just to test the loop
-    
-    if step % 5 == 0:
+print('Sim starting: ')
+for step in range(1200): # 1200 for test purposes. 
+    obs, reward, term, trunc, info = env.step(env.action_space.sample())
+    # total_reward += 1 # just to test the loop
+    total_reward += reward
+
+    if step % 20 == 0:
         print(f'Step {step}: total reward {total_reward} -------')
-    if done:
+    if term:
         print(f'Step {step}: Game Over, total reward {total_reward}')
+
+    if reward > 0:
+        print(f'Step {step}: Team scored --> Reward: {reward}, Total Reward: {total_reward}')
+        
